4424256722dc1b1589a3cf636007c653615b2690
Gaurav Singh <gauravsingh1@fb.com>
2022-01-13 21:21:29 +0000

    initial commit

---
+++ app/src/main/java/test/fb/com/android_basics_debugging/MainActivity.java
@@ -0,0 +1,14 @@
+package test.fb.com.android_basics_debugging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
