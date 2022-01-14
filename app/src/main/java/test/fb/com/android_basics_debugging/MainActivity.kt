ac07dd8f5daf6b8b22f5c4b950e8fe2318f19930
Gaurav Singh <gauravsingh1@fb.com>
2022-01-14 09:09:20 +0000

    Added different log levels example

--- app/src/main/java/test/fb/com/android_basics_debugging/MainActivity.kt
+++ app/src/main/java/test/fb/com/android_basics_debugging/MainActivity.kt
@@ -14,7 +14,11 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun logging() {
-        Log.v(TAG, "Hello world!")
+        Log.e(TAG, "ERROR: a serious error like an app crash")
+        Log.w(TAG, "WARN: warns about the potential for serious errors")
+        Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
+        Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
+        Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
 
 
