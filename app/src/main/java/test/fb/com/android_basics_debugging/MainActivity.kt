0f4c426855ae2c35738455a0327fa30cbde61e15
Gaurav Singh <gauravsingh1@fb.com>
2022-01-20 09:17:12 +0000

    Set result of division to division_textview label.

--- app/src/main/java/test/fb/com/android_basics_debugging/MainActivity.kt
+++ app/src/main/java/test/fb/com/android_basics_debugging/MainActivity.kt
@@ -2,6 +2,7 @@ package test.fb.com.android_basics_debugging
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 private const val TAG = "MainActivity"
@@ -10,16 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         logging()
+        division()
     }
 
-    fun logging() {
+    private fun division() {
+        val numerator = 60
+        var denominator = 4
+
+        // Replace 5 with 4 to fix the division by zero error and prevent the app from
+        // crashing
+        repeat(4) {
+            Thread.sleep(1)
+            findViewById<TextView>(R.id.division_textview).text = "${numerator / denominator}"
+
+            denominator--
+        }
+    }
+
+    private fun logging() {
         Log.e(TAG, "ERROR: a serious error like an app crash")
         Log.w(TAG, "WARN: warns about the potential for serious errors")
         Log.i(TAG, "INFO: reporting technical information, such as an operation succeeding")
         Log.d(TAG, "DEBUG: reporting technical information useful for debugging")
         Log.v(TAG, "VERBOSE: more verbose than DEBUG logs")
     }
-
-
 }
